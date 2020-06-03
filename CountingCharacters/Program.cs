4cb2036ad73654f963cd5b86de904d513d1a9e1d
William Morrison <williamchrismorrison@gmail.com>
2020-06-03 12:35:58 -0700

    created loop that counts, removes, and adds chars from string to dictionary

--- CountingCharacters/Program.cs
+++ CountingCharacters/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace CountingCharacters
 {
@@ -8,14 +10,28 @@ namespace CountingCharacters
         {
             Console.WriteLine("Enter a word: ");
             string inputString = Console.ReadLine();
-            int length = inputString.Length;
+            Dictionary<char, int> charsInString = new Dictionary<char, int>();
 
-            Console.WriteLine("Enter a character: ");
-            string inputChar = Console.ReadLine();
-            string inputStringWithoutInputChar = inputString.Replace(inputChar, "");
-            int stringLengthWithoutInputChar = inputStringWithoutInputChar.Length;
+            //the following loop attempts to reduce the amount of iterations by removing characters that have already been evaluated
+            //is not case-insensitve and includes non-alphabetical characters for now
+            while (inputString.Length > 0)
+            {
+                int counter = 0;
+                for (int i = 0; i < inputString.Length; i++)
+                {
+                    if (inputString[0] == inputString[i])
+                    {
+                        counter++;
+                    }
+                }
+                charsInString.Add(inputString[0], counter);
+                inputString = inputString.Replace(inputString[0].ToString(), "");
+            }
 
-
+            foreach (KeyValuePair<char, int> charInString in charsInString)
+            {
+                Console.WriteLine(charInString.Key + " : " + charInString.Value);
+            }
         }
     }
 }
