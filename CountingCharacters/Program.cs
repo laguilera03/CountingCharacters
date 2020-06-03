49e0ca5a48a143bb37dc84d656155d359766367a
William Morrison <williamchrismorrison@gmail.com>
2020-06-03 09:49:14 -0700

    Add project files.

---
+++ CountingCharacters/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CountingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word: ");
+            string inputString = Console.ReadLine();
+            int length = inputString.Length;
+
+            Console.WriteLine("Enter a character: ");
+            string inputChar = Console.ReadLine();
+            string inputStringWithoutInputChar = inputString.Replace(inputChar, "");
+            int stringLengthWithoutInputChar = inputStringWithoutInputChar.Length;
+
+
+        }
+    }
+}
